c256e9092ac0433cdadffa2ba979458c556ed5f3
Luís Mendes <luisgbm@gmail.com>
2021-02-03 17:03:01 -0300

    Brief app instructions in the login page

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -30,6 +30,9 @@ const useStyles = makeStyles(theme => ({
     },
     appBarTitle: {
         flexGrow: 1
+    },
+    marginBottom: {
+        marginBottom: theme.spacing(3)
     }
 }));
 
@@ -140,9 +143,35 @@ const Login = (props) => {
                         color='primary'
                         startIcon={<VpnKeyIcon/>}
                         size='large'
+                        className={classes.marginBottom}
                     >
                         Login
                     </Button>
+                    <br/>
+                    <Typography variant='h5' className={classes.marginBottom}>How to use the app</Typography>
+                    <Typography className={classes.marginBottom}>Finance lets you keep track of your transactions. But
+                        before we get there, we need to setup a few things first.</Typography>
+                    <Typography variant='h6' className={classes.marginBottom}>Categories</Typography>
+                    <Typography className={classes.marginBottom}>The first thing to do when you log into Finance is to
+                        create a few categories.</Typography>
+                    <Typography className={classes.marginBottom}>Categories can be of two types: Expenses and Incomes.
+                        Expenses are things you spend money on, like Food, or your Car. Incomes are things from which
+                        you earn money, like the Salary from your job, or Earnings from the stock market.</Typography>
+                    <Typography variant='h6' className={classes.marginBottom}>Accounts</Typography>
+                    <Typography className={classes.marginBottom}>Transactions belong into accounts, so the next thing to
+                        do is to create a few accounts. These can be your Bank account, your Credit Card, or even your
+                        Wallet for cash transactions.</Typography>
+                    <Typography variant='h6' className={classes.marginBottom}>Transactions</Typography>
+                    <Typography className={classes.marginBottom}>With categories and accounts, you can now create a
+                        transaction. A transaction has a value, a category, a date, and optionally a description. If you
+                        create an Expense transaction, it will subtract money from your account. If you create an Income
+                        transaction, it will add money to your account. The account keeps track of all your transactions
+                        and calculates the balance.</Typography>
+                    <Typography variant='h6' className={classes.marginBottom}>Transfers</Typography>
+                    <Typography className={classes.marginBottom}>Transfers are a special kind of transactions. They
+                        don't have a category, but they have an origin account (From) and a destination account (To),
+                        and also optionally a description. When you create a transfer, it will subtract money from the
+                        origin account, and add money to the destination account.</Typography>
                 </form>
             </Container>
         </>
